916608fe46eaf43f22384907998f0bb4ae044b6f
KHeap25 <-->
2020-06-12 14:29:33 +0200

    further steps done

--- dataset/create_mapping_lists.py
+++ dataset/create_mapping_lists.py
@@ -5,8 +5,9 @@
 #
 #Changes in compartion to create_mapping_list.py
 #-select train, val, test at the beginning
-#-reduce the number of for loop iterations in func writeNamesInFile()
-#
+#-reduce the number of for loop iterations in func createMappingList()
+#-cut datasetPath from the string which is written in the lists
+#-add func createTrainValMappingList()
 #
 # python imports
 from __future__ import print_function, absolute_import, division
@@ -26,17 +27,18 @@ def main():
 
     
     # how to search for the pathnames
-    # all *_polygon.json files in the annotations folder
+    # all *_labelTrainIds.png files in the annotations folder
     # all *_raw images in the original_images folder
     # separated by train, val and test 
     
-    search_annotations_train = os.path.join( datasetPath, "annotations/train" , "*_labelTrainIds.png" ) #after testing search directly for _labelTrainIds.png
-    search_annotations_val = os.path.join( datasetPath , "annotations/val" , "*_labelTrainIds.png" ) #after testing change to _raw.png
-    search_annotations_test = os.path.join( datasetPath , "annotations/test" , "*_labelTrainIds.png" )
+    os.chdir(datasetPath)
+    search_annotations_train = os.path.join( "annotations/train" , "*_labelTrainIds.png" ) #after testing search directly for _labelTrainIds.png
+    search_annotations_val = os.path.join( "annotations/val" , "*_labelTrainIds.png" ) #after testing change to _raw.png
+    search_annotations_test = os.path.join( "annotations/test" , "*_labelTrainIds.png" )
 
-    search_raw_train = os.path.join( datasetPath, "original_images/train" , "*_raw.png" )
-    search_raw_val = os.path.join( datasetPath , "original_images/val" , "*_raw.png" )
-    search_raw_test = os.path.join( datasetPath , "original_images/test" , "*_raw.png" )
+    search_raw_train = os.path.join( "original_images/train" , "*_raw.png" )
+    search_raw_val = os.path.join( "original_images/val" , "*_raw.png" )
+    search_raw_test = os.path.join( "original_images/test" , "*_raw.png" )
 
     # search and sort the lists with the file names
 
@@ -57,34 +59,40 @@ def main():
     #parse filesRaw and filesAnnotation and store the names pairwise in the corresponding text file 
     #first for training dataset
     try:
-        writeNamesInFile(filesRawTrain, filesAnnotationsTrain, "train", datasetPath)
+        createMappingList(filesRawTrain, filesAnnotationsTrain, "train", datasetPath)
     except:
-        print("error: creation of train_mapping_list failed")
+        print("error: creation of train_mapping_list.txt failed")
 
     #second for validation dataset
     try:
-        writeNamesInFile(filesRawVal, filesAnnotationsVal, "val", datasetPath)
+        createMappingList(filesRawVal, filesAnnotationsVal, "val", datasetPath)
     except:
-        print("error: creation of val_mapping_list failed")
+        print("error: creation of val_mapping_list.txt failed")
 
     #third for test dataset
     try:
-        writeNamesInFile(filesRawTest, filesAnnotationsTest, "test", datasetPath)
+        createMappingList(filesRawTest, filesAnnotationsTest, "test", datasetPath)
     except:
-        print("error: creation of test_mapping_list failed")
+        print("error: creation of test_mapping_list.txt failed")
 
+    #create the train_val_mapping_list.txt file
+    try:
+        createTrainValMappingList(filesRawTrain, filesRawVal, filesAnnotationsTrain, filesAnnotationsVal, datasetPath)
+    except:
+        print("error: creation of train_val_mapping_list.txt failed")
 
-#Func: writeNamesInFile()################################################
-#This function parses the annotation list with attention to dataset.
-#It creates the mapping_file and stores the pair of raw file name 
-#and annotation file name in ecach line of the mapping file.
+#Func: createMappingList()################################################
+#This function parses the raw list with attention to dataset type.
+#It creates the mapping_file for train, val or test dataset and stores the 
+#pair of raw file name and coresponding annotation file name in ecach line 
+#of the mapping_list file.
 #Params:
 #raw: list of raw data file names
 #annotation: list of annotation file names
 #dataset: type of the dataset (train, val or test)
 #datasetPath: path of the dataset folder (from env variable) 
 ##########################################################################
-def writeNamesInFile(raw, annotation, dataset, datasetPath):
+def createMappingList(raw, annotation, dataset, datasetPath):
 
     if annotation != [] and raw != []:
         if dataset=="train":
@@ -153,6 +161,46 @@ def getNameFromPath(dataset_path, format):
     name=name[cut:]
     return name
 
+#Func: createTrainValMappingList()########################################
+#This function generates the train_val_mapping_list.txt
+#The pairs of train data names and val data names are stored in this list.
+#
+#Params: 
+#train_raw: list of train raw data file names
+#val_raw: list of val raw data file names
+#train_annot: list of train annotation data file names
+#val_annot: list of val annotation data file names
+#datasetPath: path of the dataset folder (from env variable) 
+##########################################################################
+def createTrainValMappingList(train_raw, val_raw, train_annot, val_annot, datasetPath):
+    
+    if train_raw!= [] and val_raw != [] and train_annot != [] and  val_annot != []:
+
+        mapping_file_path = datasetPath+"/train_val_mapping_list.txt"
+        mapping_file = open(mapping_file_path,'w')
+
+        for i in train_raw:
+            if "train" in i:
+                mapping_file.write(i+" ")
+                search_partner = getNameFromPath(i, "raw.png")
+
+                for j in train_annot:
+                    if "train" in j:
+                        if search_partner in j:
+                            mapping_file.write(j+"\n")
+                            break
+        
+        for i in val_raw:
+            if "val" in i:
+                mapping_file.write(i+" ")
+                search_partner = getNameFromPath(i, "raw.png")
+
+                for j in val_annot:
+                    if "val" in j:
+                        if search_partner in j:
+                            mapping_file.write(j+"\n")
+                            break
+        mapping_file.close                
 
 # call the main
 if __name__ == "__main__":
